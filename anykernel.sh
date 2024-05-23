00e737f4f2e7047c4cdaec792ad39d339d863ed5
Hakan <hknhzl@gmail.com>
2024-05-23 14:44:45 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Sleepy Kernel for Redmi Note 12 Pro 4G by @ itsshashanksp
+kernel.string=Positron Kernel for Redmi Note 12 Pro 4G by @ HaKaN
 do.devicecheck=1
 do.modules=0
 do.systemless=1

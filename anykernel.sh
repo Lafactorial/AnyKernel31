49d1c3fca063f6c30af34f0d26abb269a928ea3a
Hakan <hknhzl@gmail.com>
2026-09-21 16:36:28 +0300

    Update AnyKernel properties and boot variables
    Modified device properties and boot variables for AnyKernel setup.

--- anykernel.sh
+++ anykernel.sh
@@ -5,37 +5,39 @@
 # global properties
 properties() { '
 kernel.string=Kernel for Garnet by @ HaKaN
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=garnet
-device.name2=garnet_in
-device.name3=garnetp
-device.name4=Redmi Note 13 Pro 5G
-device.name5=Poco X6 5G
-supported.versions=16
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
 
 # boot shell variables
-BLOCK=boot
-IS_SLOT_DEVICE=1
-RAMDISK_COMPRESSION=auto
-PATCH_VBMETA_FLAG=auto
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
 ## Start boot install
-
-split_boot # Use split_boot to skip ramdisk unpack, e.g., for devices with init_boot ramdisk
-
-ui_print "- $(strings "${AKHOME}"/Image 2>/dev/null | grep -E -m1 'Linux version.*#' | awk '{print $3}')"
-
-flash_boot # Use flash_boot to skip ramdisk repack, e.g., for devices with init_boot ramdisk
+split_boot
+if [ -f "split_img/ramdisk.cpio" ]; then
+    unpack_ramdisk
+    write_boot
+else
+    flash_boot
+fi
 
 ## End boot install
